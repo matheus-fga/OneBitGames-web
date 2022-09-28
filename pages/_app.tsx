e3fcdb51e8decca986260fd78dbb089a130047c4
Matheus Silva <matheussilva.fga@gmail.com>
2022-09-28 09:51:57 -0300

    Installing and configuring Redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,24 @@
 import React from 'react'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from '../store';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles/globals.css'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return(
-    <>
-      <Head>
-        <title>OneBitGames</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <Component {...pageProps} />
-    </>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Head>
+          <title>OneBitGames</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </Head>
+        <Component {...pageProps} />
+      </PersistGate>
+    </Provider> 
   )
 }
 
